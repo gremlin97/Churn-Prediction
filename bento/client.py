6e29241e23f49762dfb775ce4102829e764a4099
Kunal Kasodekar <kasodekarsunil@gmail.com>
2024-06-05 23:15:59 +0000

    Minor addition

--- bento/client.py
+++ bento/client.py
@@ -19,3 +19,23 @@ with bentoml.SyncHTTPClient("http://localhost:3000") as client:
     )
 
 print(result)
+
+'''
+Json Request:
+
+{
+        "data": {
+            "CreditScore": [668, 664],
+            "Geography": ["France", "France"],
+            "Gender": ["Male", "Male"],
+            "Age": [33.0, 34.0],
+            "Tenure": [3, 4],
+            "Balance": [0.0, 1.0],
+            "NumOfProducts": [2, 3],
+            "HasCrCard": [1.0, 3],
+            "IsActiveMember": [0.0, 0.0],
+            "EstimatedSalary": [181449.97, 32242.46]
+        }
+}
+
+'''
